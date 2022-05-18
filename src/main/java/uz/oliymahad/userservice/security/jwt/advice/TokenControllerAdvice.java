d56a388b9bd12a60669afe864696f561f30418d1
pheonix <exe.sirius11@gmail.com>
2022-05-18 22:30:53 +0500

    jwt done with user and config

---
+++ src/main/java/uz/oliymahad/userservice/security/jwt/advice/TokenControllerAdvice.java
@@ -0,0 +1,24 @@
+package uz.oliymahad.userservice.security.jwt.advice;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import uz.oliymahad.userservice.security.jwt.exception.TokenRefreshException;
+
+import java.util.Date;
+
+@RestControllerAdvice
+public class TokenControllerAdvice {
+
+  @ExceptionHandler(value = TokenRefreshException.class)
+  @ResponseStatus(HttpStatus.FORBIDDEN)
+  public ErrorMessage handleTokenRefreshException(TokenRefreshException ex, WebRequest request) {
+    return new ErrorMessage(
+        HttpStatus.FORBIDDEN.value(),
+        new Date(),
+        ex.getMessage(),
+        request.getDescription(false));
+  }
+}
---
+++ src/main/java/uz/oliymahad/userservice/security/jwt/models/ERole.java
@@ -0,0 +1,7 @@
+package uz.oliymahad.userservice.security.jwt.models;
+
+public enum ERole {
+  ROLE_USER,
+  ROLE_MODERATOR,
+  ROLE_ADMIN
+}
