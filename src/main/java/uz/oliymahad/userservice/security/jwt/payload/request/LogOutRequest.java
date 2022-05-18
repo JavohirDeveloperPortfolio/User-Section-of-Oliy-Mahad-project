cf549c4e92de3c7fbbe72240509456d1d7404a4f
pheonix <exe.sirius11@gmail.com>
2022-05-18 22:30:53 +0500

    jwt done with user and config

---
+++ src/main/java/uz/oliymahad/userservice/security/jwt/payload/request/LogOutRequest.java
@@ -0,0 +1,9 @@
+package uz.oliymahad.userservice.security.jwt.payload.request;
+
+public class LogOutRequest {
+  private Long userId;
+
+  public Long getUserId() {
+    return this.userId;
+  }
+}
