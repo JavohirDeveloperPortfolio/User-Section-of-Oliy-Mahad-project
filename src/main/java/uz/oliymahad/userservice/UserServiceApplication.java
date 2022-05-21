587f913b478c45a16a5be2c2de5a2dd56a2c060e
JavohirDeveloperPortfolio <haqberdiyev99@bk.ru>
2022-05-21 13:25:59 +0500

    Lord edit

--- src/main/java/uz/oliymahad/userservice/UserServiceApplication.java
+++ src/main/java/uz/oliymahad/userservice/UserServiceApplication.java
@@ -2,11 +2,13 @@ package uz.oliymahad.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class UserServiceApplication {
 
     public static void main(String[] args) {
