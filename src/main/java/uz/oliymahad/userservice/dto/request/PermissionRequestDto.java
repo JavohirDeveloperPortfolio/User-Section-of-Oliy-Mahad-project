1be9b86d36cafc33351ff49156e728c80270eb6e
davronismoilov <ismoilovdavron16@gmail.com>
2022-06-03 12:32:11 +0500

    image upload

--- src/main/java/uz/oliymahad/userservice/dto/request/PermissionRequestDto.java
+++ src/main/java/uz/oliymahad/userservice/dto/request/PermissionRequestDto.java
@@ -3,12 +3,13 @@ package uz.oliymahad.userservice.dto.request;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import uz.oliymahad.userservice.model.enums.ERole;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class PermissionRequestDto {
-    private String roleName;
+    private ERole roleName;
     private boolean visibility;
     private boolean editable;
     private boolean delete;
