42f84df24759d8dddda9f39d1c90c43ee981779a
User <bekhzod.asadullayev@gmail.com>
2022-07-20 08:59:36 +0500

    user roles and permissions fixed

--- src/main/java/uz/oliymahad/userservice/repository/QueueRepository.java
+++ src/main/java/uz/oliymahad/userservice/repository/QueueRepository.java
@@ -16,9 +16,8 @@ public interface QueueRepository extends JpaRepository<QueueEntity,Long> {
     @Query(value = "select rank from (select user_id,applieddate, rank() over (order by applieddate) from queue_entity where course_id = :courseId) as sub where user_id = :userId", nativeQuery = true)
     List<Long> getUserCourseQueue(@Param("userId") Long userId, @Param("courseId") Long courseId);
 
-    @Query(value = "select * from (select *,RANK() over(order by applieddate asc) from queue_entity where course_id = :courseId and status = :status and gender = :gender ) as sub  limit :limit", nativeQuery = true)
+    @Query(value = "select * from (select *, RANK() over(order by applieddate asc) from queue_entity where course_id = :courseId and status = :status and gender = :gender ) as sub  limit :limit", nativeQuery = true)
     List<Long> filterByCourseStatusGenderLimitForGroups(@Param("courseId") Long courseId, @Param("status") String status, @Param("gender") String gender, @Param("limit") Long limit);
-
 
     @Query(value = "select *from filter_all_parameters1(i_user_id := :userId, i_gender := :gender, i_status := :status, i_course_id := :courseId)",nativeQuery = true)
     List<QueueEntity> getQueueByFilter(
